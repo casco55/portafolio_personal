914f2e1886b8fad85451e0622bfb543cfaa0ed40
casco55 <juanjo.desarrollador@gmail.com>
2021-04-26 12:28:10 -0400

    first portafolio 1.0 hasta descripción

---
+++ js/main.js
@@ -0,0 +1,17 @@
+//Iniciar scrollspy
+$('body').scrollspy({target: '#menu-navegacion'});
+
+//Scroll suavizado
+$('#menu-navegacion a').on('click', function(event){
+    if(this.hash != ""){
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function(){
+            window.location.hash = hash;
+        });
+    }
+})
